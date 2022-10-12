61bd28568b13a4e0ee1dd6707b2451bca2853b43
GermanVelandia <german.velandia@swissaxis.ch>
2022-10-12 17:19:02 -0500

    Ahora imprime el texto del twee en consola

--- index.py
+++ index.py
@@ -7,9 +7,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from pprint import pprint
-import mysql.connector
-import time 
-import conexion
+#import mysql.connector
+#import time 
+#import conexion
 def waitUntilReady(driver, delay, by, value_by):
     try:
         myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((by, value_by)))
@@ -23,9 +23,12 @@ def getTweets(user):
     driver.get("https://twitter.com/" + user)
     waitUntilReady(driver, 5, By.CSS_SELECTOR, "div[data-testid=cellInnerDiv]")
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    elements = driver.find_elements(By.CSS_SELECTOR, "div[data-testid=cellInnerDiv]")
-    print(elements.text)
-    set(elements)
+    elements = driver.find_elements(By.CSS_SELECTOR, "[data-testid='tweet']")
+    #elements = driver.find_elements(By.CSS_SELECTOR, "div[data-testid=cellInnerDiv]")
+    for element in elements:
+        print(element.text)
+    #print(elements.text)
+    #set(elements)
 
 users = (["@ManuelTurizoMTZ","@andrescepeda","@Greeicy_rendon", "@TheRock" , "@J_Rodrigues99"])    
 for user in users: 
